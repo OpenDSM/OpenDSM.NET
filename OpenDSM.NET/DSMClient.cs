900c2206d6c415b8822685a4a60e1474e641e8a0
dcmanproductions <dcmanproductions@gmail.com>
2022-09-12 12:24:36 -0400

    Moved Host constant to Requests

--- OpenDSM.NET/DSMClient.cs
+++ OpenDSM.NET/DSMClient.cs
@@ -1,14 +1,7 @@
-global using static OpenDSM.NET.DSMClient;
 namespace OpenDSM.NET;
 public record UserCredentials(string username, string token);
 public class DSMClient
 {
-    /// <summery>The host url</summery>
-#if DEBUG
-    public static readonly string HOST = "http://127.0.0.1:8080";
-#else
-    public static readonly string HOST = "http://api.opendsm.tk";
-#endif
     private UserCredentials credentials;
     internal readonly HttpClient client;
 
