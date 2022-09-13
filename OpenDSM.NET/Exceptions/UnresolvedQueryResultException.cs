84f8b79a44e2b53ed06109937131712d6926376e
dcmanproductions <dcmanproductions@gmail.com>
2022-09-12 21:44:18 -0400

    added message null check

--- OpenDSM.NET/Exceptions/UnresolvedQueryResultException.cs
+++ OpenDSM.NET/Exceptions/UnresolvedQueryResultException.cs
@@ -6,6 +6,6 @@ namespace OpenDSM.NET.Exceptions;
 public class UnresolvedQueryResultException : Exception
 {
     public UnresolvedQueryResultException(string message = "") :
-    base($"The query posted returned with no results.  This usually means that the query was invalid or the information requested was not available.\n{message}")
+    base($"The query posted returned with no results.  This usually means that the query was invalid or the information requested was not available.{(string.IsNullOrWhiteSpace(message) ? "" : $"\n{message}")}")
     { }
 }
