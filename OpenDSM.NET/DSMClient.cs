5690c7521ca4ec2fd733c91e66871c2ac10514e7
dcmanproductions <dcmanproductions@gmail.com>
2022-09-13 09:59:46 -0400

    Merged Requests into DSMClient

--- OpenDSM.NET/DSMClient.cs
+++ OpenDSM.NET/DSMClient.cs
@@ -1,15 +1,22 @@
 using Newtonsoft.Json.Linq;
 using OpenDSM.NET.Exceptions;
+using OpenDSM.NET.Requests;
 
 namespace OpenDSM.NET;
 public record UserCredentials(string username, string token);
 /// <summary>
 /// The client that will make all of the requests
 /// </summary>
-public class DSMClient
+public sealed class DSMClient
 {
-    private UserCredentials credentials;
+    private readonly UserCredentials credentials;
     internal readonly HttpClient client;
+    public readonly UserRequests Users;
+#if DEBUG
+    private readonly string HOST = "http://127.0.0.1:8080";
+#else
+    private readonly string HOST = "http://api.opendsm.tk";
+#endif
 
     /// <summary>
     /// The client that will make all of the requests
@@ -23,6 +30,7 @@ public class DSMClient
         {
             throw new InvalidCredentialsException(credentials, error);
         }
+        Users = new(this);
     }
 
     private HttpClient GetClient()
@@ -36,7 +44,7 @@ public class DSMClient
 
     private bool ValidateCredentials(UserCredentials credentials, out string error)
     {
-        using var response = Post(this, "/user/validate/token", new KeyValuePair<string, string>[]
+        using var response = Post("/user/validate/token", new KeyValuePair<string, string>[]
         {
             new KeyValuePair<string, string>("username", credentials.username),
             new KeyValuePair<string, string>("token", credentials.token)
@@ -50,5 +58,37 @@ public class DSMClient
         }
         error = $"Server responded with status code {response.StatusCode}";
         return false;
+    }
+
+
+
+    // GET DATA
+    internal HttpResponseMessage Get(string path) => Make(HttpMethod.Get, path);
+    internal HttpResponseMessage Get(string path, IEnumerable<KeyValuePair<string, string>>? formData = null) => Make(HttpMethod.Get, path, formData: formData);
+
+    // POST DATA
+    internal HttpResponseMessage Post(string path) => Make(HttpMethod.Post, path);
+    internal HttpResponseMessage Post(string path, string body) => Make(HttpMethod.Post, path, body);
+    internal HttpResponseMessage Post(string path, IEnumerable<KeyValuePair<string, string>>? formData) => Make(HttpMethod.Post, path, formData: formData);
+
+    // DELETE DATA
+    internal HttpResponseMessage Delete(string path) => Make(HttpMethod.Delete, path);
+    internal HttpResponseMessage Delete(string path, string body) => Make(HttpMethod.Delete, path, body);
+    internal HttpResponseMessage Delete(string path, IEnumerable<KeyValuePair<string, string>>? formData = null) => Make(HttpMethod.Delete, path, formData: formData);
+
+    // PATCH DATA
+    internal HttpResponseMessage Patch(string path) => Make(HttpMethod.Patch, path);
+    internal HttpResponseMessage Patch(string path, string body) => Make(HttpMethod.Patch, path, body);
+    internal HttpResponseMessage Patch(string path, IEnumerable<KeyValuePair<string, string>>? formData = null) => Make(HttpMethod.Patch, path, formData: formData);
+
+    private HttpResponseMessage Make(HttpMethod method, string path, string? body = "", IEnumerable<KeyValuePair<string, string>>? formData = null)
+    {
+        using HttpRequestMessage request = new(method, $"{HOST}{path}");
+        if (!string.IsNullOrWhiteSpace(body))
+            request.Content = new StringContent(body, System.Text.Encoding.UTF8);
+        else if (formData != null)
+            request.Content = new FormUrlEncodedContent(formData);
+        HttpResponseMessage response = client.Send(request);
+        return response;
     }
 }
