31034a4591db7a1d9d2bc5731965351e6be0307a
dcmanproductions <dcmanproductions@gmail.com>
2022-09-13 20:12:46 -0400

    Changed to use api_key

--- OpenDSM.NET/DSMClient.cs
+++ OpenDSM.NET/DSMClient.cs
@@ -1,15 +1,12 @@
-using Newtonsoft.Json.Linq;
-using OpenDSM.NET.Exceptions;
 using OpenDSM.NET.Requests;
 
 namespace OpenDSM.NET;
-public record UserCredentials(string username, string token);
 /// <summary>
 /// The client that will make all of the requests
 /// </summary>
 public sealed class DSMClient
 {
-    private readonly UserCredentials credentials;
+    private readonly string api_key;
     internal readonly HttpClient client;
     public readonly UserRequests Users;
 #if DEBUG
@@ -22,64 +19,39 @@ public sealed class DSMClient
     /// The client that will make all of the requests
     /// </summary>
     /// <param name="credentials">The users credentials</param>
-    public DSMClient(UserCredentials credentials)
+    public DSMClient(string api_key)
     {
-        this.credentials = credentials;
+        this.api_key = api_key;
         client = GetClient();
-        if (!ValidateCredentials(credentials, out string error))
-        {
-            throw new InvalidCredentialsException(credentials, error);
-        }
         Users = new(this);
     }
 
     private HttpClient GetClient()
     {
         HttpClient client = new();
-        client.DefaultRequestHeaders.Add("auth_user", credentials.username);
-        client.DefaultRequestHeaders.Add("auth_token", credentials.token);
+        client.DefaultRequestHeaders.Add("api_key", api_key);
 
         return client;
     }
 
-    private bool ValidateCredentials(UserCredentials credentials, out string error)
-    {
-        using var response = Post("/user/validate/token", new KeyValuePair<string, string>[]
-        {
-            new KeyValuePair<string, string>("username", credentials.username),
-            new KeyValuePair<string, string>("token", credentials.token)
-        });
-        if (response.IsSuccessStatusCode)
-        {
-            JObject json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
-            bool success = (bool)json["success"];
-            error = (string)json["message"];
-            return success;
-        }
-        error = $"Server responded with status code {response.StatusCode}";
-        return false;
-    }
-
-
-
     // GET DATA
     internal HttpResponseMessage Get(string path) => Make(HttpMethod.Get, path);
-    internal HttpResponseMessage Get(string path, IEnumerable<KeyValuePair<string, string>>? formData = null) => Make(HttpMethod.Get, path, formData: formData);
+    internal HttpResponseMessage Get(string path, IEnumerable<KeyValuePair<string, string>> formData) => Make(HttpMethod.Get, path, formData: formData);
 
     // POST DATA
     internal HttpResponseMessage Post(string path) => Make(HttpMethod.Post, path);
     internal HttpResponseMessage Post(string path, string body) => Make(HttpMethod.Post, path, body);
-    internal HttpResponseMessage Post(string path, IEnumerable<KeyValuePair<string, string>>? formData) => Make(HttpMethod.Post, path, formData: formData);
+    internal HttpResponseMessage Post(string path, IEnumerable<KeyValuePair<string, string>> formData) => Make(HttpMethod.Post, path, formData: formData);
 
     // DELETE DATA
     internal HttpResponseMessage Delete(string path) => Make(HttpMethod.Delete, path);
     internal HttpResponseMessage Delete(string path, string body) => Make(HttpMethod.Delete, path, body);
-    internal HttpResponseMessage Delete(string path, IEnumerable<KeyValuePair<string, string>>? formData = null) => Make(HttpMethod.Delete, path, formData: formData);
+    internal HttpResponseMessage Delete(string path, IEnumerable<KeyValuePair<string, string>> formData) => Make(HttpMethod.Delete, path, formData: formData);
 
     // PATCH DATA
     internal HttpResponseMessage Patch(string path) => Make(HttpMethod.Patch, path);
     internal HttpResponseMessage Patch(string path, string body) => Make(HttpMethod.Patch, path, body);
-    internal HttpResponseMessage Patch(string path, IEnumerable<KeyValuePair<string, string>>? formData = null) => Make(HttpMethod.Patch, path, formData: formData);
+    internal HttpResponseMessage Patch(string path, IEnumerable<KeyValuePair<string, string>> formData) => Make(HttpMethod.Patch, path, formData: formData);
 
     private HttpResponseMessage Make(HttpMethod method, string path, string? body = "", IEnumerable<KeyValuePair<string, string>>? formData = null)
     {
@@ -89,6 +61,7 @@ public sealed class DSMClient
         else if (formData != null)
             request.Content = new FormUrlEncodedContent(formData);
         HttpResponseMessage response = client.Send(request);
+        if(response.StatusCode > System.Net.HttpStatusCode.BadRequest)throw new HttpRequestException($"Server returned with status code {response.StatusCode:D} {response.StatusCode:G}");
         return response;
     }
 }
